3969c9fd1673d8fcde2de8dbe1e622b4eef4e610
Денис Громачихин <denisgromacihin@MacBook-Air-de-Denis.local>
2024-04-15 11:21:32 +0300

    -add src and gitignore

---
+++ src/client/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+
+int main(){
+    std::cout<<"client\n";
+    return 0;
+}
---
+++ src/server/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+
+int main(){
+    std::cout<<"server\n";
+    return 0;
+}
